a6ad6d4a46aec6bf2276777935e1579ea59a28bf
Georgi Tomov <georgi_tomov_qa@yahoo.com>
2025-02-16 16:45:14 +0200

    Homework3 ready

--- Homework3/Car.java
+++ Homework3/Car.java
@@ -1,4 +1,32 @@
 package Homework3;
 
 public class Car {
+    int year;
+    double price;
+    boolean isSportCar;
+    int fuelTankCapacity;
+    double freeFuel;
+    String engineFuelOperationSystem;
+
+    Car(int year, double price, boolean isSportCar, int fuelTankCapacity,
+        double freeFuel, String engineFuelOperationSystem) {
+        this.year = year;
+        this.price = price;
+        this.isSportCar = isSportCar;
+        this.fuelTankCapacity = fuelTankCapacity;
+        this.freeFuel = freeFuel;
+        this.engineFuelOperationSystem = engineFuelOperationSystem;
+    }
+
+    void changeEngineFuelOperationSystem(String newEngineOperatingSystem) {
+        this.engineFuelOperationSystem = newEngineOperatingSystem;
+    }
+
+    void useFuel(double fuel) {
+        if (fuel > freeFuel) {
+            System.out.println("Not enough free fuel!");
+        } else {
+            freeFuel -= fuel;
+        }
+    }
 }
--- Homework3/CarPerson.java
+++ Homework3/CarPerson.java
@@ -1,4 +1,21 @@
 package Homework3;
 
 public class CarPerson {
+    public static void main(String[] args) {
+        Car lada = new Car(2000, 5000.00, false,
+                80, 40, "Petrol");
+        Car moskvich = new Car(2010, 5800.00, true,
+                50, 25, "Gas");
+
+        lada.useFuel(35.00);
+        moskvich.changeEngineFuelOperationSystem("Diesel");
+
+        System.out.println(lada.year + " " + lada.price + " " + lada.isSportCar + " " +
+                        lada.fuelTankCapacity + " " + lada.freeFuel + " " +
+                        lada.engineFuelOperationSystem);
+
+        System.out.println(moskvich.year + " " + moskvich.price + " " + moskvich.isSportCar
+                        + " " + moskvich.fuelTankCapacity + " " + moskvich.freeFuel
+                        + " " + moskvich.engineFuelOperationSystem);
+    }
 }
